5937202c9ffb930928b473a22be17be32e51e70c
Devin Walter <jwaltede@jci.com>
2019-07-18 14:30:05 -0400

    started on component workflow to test something

---
+++ src/components/pages/LoggedInIndex/LoggedInIndex.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './LoggedInIndex.scss';
+
+export default class LoggedInIndex extends Component { 
+
+    render() { 
+        return ( 
+            <div>LoggedInIndex</div>
+        );
+    }
+}
---
+++ src/components/pages/LoggedOutIndex/LoggedOutIndex.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'; 
+import './LoggedOutIndex.scss';
+
+export default class LoggedOutIndex extends React.Component { 
+    render() {
+        return (
+            <div>LoggedOutIndex</div>
+        );
+    }
+}
